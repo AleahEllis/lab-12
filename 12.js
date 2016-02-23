2e32baba02328c52359a7baf9382237319a5b28c
Aleah Ellis <aleah.is@icloud.com>
2016-02-23 15:46:52 -0500

    made list, and made it work

---
+++ 12.js
@@ -0,0 +1,36 @@
+var app = angular.module('myModule', []);
+// This is declaring a module. More on this in a moment
+app.controller('theController', function(){
+	var vm = this;
+
+	vm.items = [
+		{info: 'Go to the grocery store', date: '05/24'},
+	    {info: 'clean the bathroom', date: '05/20'},
+	    {info: 'workout', date: '05/21'},
+	    {info: 'study', date: '05/05'},
+    ];
+
+
+	vm.displayInput=function(){
+		vm.items.push(
+			{info: vm.list, date: vm.date
+			}
+
+		)
+	}
+});
+
+ // function vm($scope) {
+ //      $scope.list = [
+ //        {item: 'Go to the grocery store'},
+ //        {item: 'clean the bathroom'},
+ //        {item: 'workout'},
+ //        {item: 'study'},
+ //      ];
+ //    }
+
+ 	// {'Go to the grocery store', 05/24}
+  // 		{'clean the bathroom', 03/31}
+  // 		{'workout', 05/06} 
+  // 		{'study', 05/18}
+  // 		 ];
